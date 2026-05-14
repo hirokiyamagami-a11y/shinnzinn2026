33835c5937947f4f3676f9e7faa736bde7a734b5
hiroki_yamagami <hiroki.yamagami@sacn.co.jp>
2026-05-14 14:06:07 +0900

    Indexからスタッフ一覧を表示できるように

---
+++ shinnzinn2026/Controllers/StaffListController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace shinnzinn2026.Controllers
+{
+    public class StaffListController : Controller
+    {
+        public IActionResult StaffList() => View();
+    }
+}
