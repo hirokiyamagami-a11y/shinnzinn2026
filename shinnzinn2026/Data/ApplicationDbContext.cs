cbf1f099386086950a4daff2eefaabcec32c73de
hiroki_yamagami <hiroki.yamagami@sacn.co.jp>
2026-05-15 10:39:24 +0900

    Appうんたらかんたらの追加

---
+++ shinnzinn2026/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using shinnzinn2026.Models;
+using System.Reflection.Emit;
+
+namespace shinnzinn2026.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Staff> Staffs { get; set; }
+
+        // もしテーブル名やカラム名を細かく調整したいならここに追加するけど、
+        // 今はモデルに [Table("staff")] とか書いてるから、ここは空っぽで大丈夫よ
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ shinnzinn2026/Models/Staff.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace shinnzinn2026.Models
+{
+    [Table("staff")]
+    public class Staff
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+
+        [Column("staff_cd")]
+        public string StaffCd { get; set; } = "";
+
+        [Column("password")]
+        public string Password { get; set; } = "";
+
+        [Column("name")]
+        public string? Name { get; set; } // DEFAULT NULL だから ? をつける
+
+        [Column("delete_flag")]
+        public short DeleteFlag { get; set; }
+
+        [Column("manager_flag")]
+        public short ManagerFlag { get; set; }
+
+        [Column("registration_time")]
+        public DateTime? RegistrationTime { get; set; }
+
+        [Column("registrant_id")]
+        public long? RegistrantId { get; set; }
+
+        [Column("updated_time")]
+        public DateTime? UpdatedTime { get; set; }
+
+        [Column("updated_id")]
+        public long? UpdatedId { get; set; }
+    }
+}
